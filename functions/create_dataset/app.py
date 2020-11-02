9ebc0b03a1898a9173a5a5e3a9e9350557b84ab1
Kazuya Iwami <kiiwami@amazon.com>
2020-11-03 01:36:25 +0900

    Use aws_lambda_powertools

--- functions/create_dataset/app.py
+++ functions/create_dataset/app.py
@@ -1,21 +1,31 @@
+"""
+Create an Amazon Forecast dataset. The information about the dataset that you provide
+helps AWS Forecast understand how to consume the data for model training.
+"""
 from os import environ
 from datetime import datetime
-from boto3 import client
-import actions
-from loader import Loader
+import boto3
+# From Lambda Layers
+import actions  # pylint: disable=import-error
+from aws_lambda_powertools import Logger  # pylint: disable=import-error
 
-ACCOUNTID = client('sts').get_caller_identity()['Account']
 DATASET_NAME = '{project_name}_{dataset_type}'
 DATASET_ARN = 'arn:aws:forecast:{region}:{account}:dataset/{dataset_name}'
-LOADER = Loader()
+
+logger = Logger()
+forecast_client = boto3.client('forecast')
+account_id = boto3.client('sts').get_caller_identity()['Account']
 
 
-def lambda_handler(event, context):
-    print(event)
-    event['AccountID'] = ACCOUNTID
-    event['CurrentDate'] = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+def lambda_handler(event, _):
+    """
+    Lambda function handler
+    """
+    logger.info({'message': 'Event received', 'event': event})
+
     datasets = event['Datasets']
-    status = None
+    event['AccountID'] = account_id
+    event['CurrentDate'] = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     event['DatasetName'] = DATASET_NAME.format(
         project_name=event['ProjectName'],
         dataset_type=datasets[0]['DatasetType']
@@ -26,19 +36,31 @@ def lambda_handler(event, context):
         dataset_name=event['DatasetName']
     )
     try:
-        status = LOADER.forecast_cli.describe_dataset(
+        response = forecast_client.describe_dataset(
             DatasetArn=event['DatasetArn']
         )
-    except LOADER.forecast_cli.exceptions.ResourceNotFoundException:
-        LOADER.logger.info('Dataset not found! Will follow to create dataset.')
+        logger.info({
+            'message': 'forecast_client.describe_dataset called',
+            'response': response
+        })
+    except forecast_client.exceptions.ResourceNotFoundException:
+        logger.info('Dataset not found. Will follow to create dataset.')
         for dataset in datasets:
-            LOADER.forecast_cli.create_dataset(
+            response = forecast_client.create_dataset(
                 **dataset,
                 DatasetName=event['DatasetName']
             )
-        status = LOADER.forecast_cli.describe_dataset(
+            logger.info({
+                'message': 'Called forecast_client.create_dataset',
+                'response': response
+            })
+        response = forecast_client.describe_dataset(
             DatasetArn=event['DatasetArn']
         )
+        logger.info({
+            'message': 'Called forecast_client.describe_dataset',
+            'response': response
+        })
 
-    actions.take_action(status['Status'])
+    actions.take_action(response['Status'])
     return event
